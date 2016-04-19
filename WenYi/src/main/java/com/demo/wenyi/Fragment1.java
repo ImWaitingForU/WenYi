ad8c8c2cae76126155166bdf6400844df20cd36e
chan <870536460@qq.com>
2016-04-19 17:24:04 +0800

    Initial commit --- by chan

---
+++ WenYi/src/main/java/com/demo/wenyi/Fragment1.java
@@ -0,0 +1,22 @@
+package com.demo.wenyi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/4/17.
+ */
+public class Fragment1 extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container,
+                              @Nullable Bundle savedInstanceState) {
+        View  view  = inflater.inflate (R.layout.fragment1,container,false);
+        return view;
+    }
+}
